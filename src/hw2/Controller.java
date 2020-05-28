dc9512a9d129b3db4c7adbfdef9547ce2d708d14
dimsuwr <soup_95@mail.ru>
2020-05-28 12:25:44 +0300

    Initial commit

---
+++ src/hw2/Controller.java
@@ -0,0 +1,26 @@
+package hw2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private Button button1;
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private TextField textField;
+
+
+    public void btn1Click() {
+        textArea.appendText(textField.getText());
+        textArea.setWrapText(true);
+        textField.setText("");
+
+
+
+    }
+}
